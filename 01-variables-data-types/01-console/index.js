bfed508a66f5a9ea80c7efde982bc159026216b1
Jim Tony <jim.tony@navis.com>
2024-07-02 20:48:24 +0530

    Explored the types of console

--- 01-variables-data-types/01-console/index.js
+++ 01-variables-data-types/01-console/index.js
@@ -1,1 +1,20 @@
-alert('Hey Console');
+// alert('Hey Console');
+console.log("Hello World");
+console.log(100);
+console.warn("Hey Be cautious");
+console.info('12');
+console.table([{firstName: 'Jim', secondName: 'Tony'}, {firstName:'Paul', secondName:'Alan'}]);
+console.error('Caused a error')
+
+
+//Create a group
+console.group('Simple group')
+console.log("Some")
+console.log("Thing")
+console.groupEnd(); //End the group
+
+
+//We can add styles for the console outputs
+
+const styles = 'padding:10px; background-color:white; color:green;'
+ console.log('%cHello world', styles)
